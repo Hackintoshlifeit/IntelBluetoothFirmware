f7ffeec0f7876b732bb53d866da03ec8614a3255
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2024-09-29 22:40:05 +0200

    Update IntelBTPatcher.cpp

--- IntelBTPatcher/IntelBTPatcher.cpp
+++ IntelBTPatcher/IntelBTPatcher.cpp
@@ -194,16 +194,14 @@ IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, 
     char hciBuf[MAX_HCI_BUF_LEN] = {0};
 
     if (data == nullptr) {
-        if (descriptor != nullptr && descriptor->getLength() > 0) {
-            IOReturn prepareResult = descriptor->prepare();
-            if (prepareResult != kIOReturnSuccess) {
-                SYSLOG(DRV_NAME, "Failed to prepare IOMemoryDescriptor: %x", prepareResult);
-                return prepareResult;
+        if (descriptor != nullptr &&
+            (getKernelVersion() < KernelVersion::Sequoia || !descriptor->prepare(kIODirectionOut))) {
+            if (descriptor->getLength() > 0) {
+                descriptor->readBytes(0, hciBuf, min(descriptor->getLength(), MAX_HCI_BUF_LEN));
+                hdrLen = (uint32_t)min(descriptor->getLength(), MAX_HCI_BUF_LEN);
             }
-
-            descriptor->readBytes(0, hciBuf, min(descriptor->getLength(), MAX_HCI_BUF_LEN));
-            hdrLen = (uint32_t)min(descriptor->getLength(), MAX_HCI_BUF_LEN);
-            descriptor->complete();
+            if (getKernelVersion() >= KernelVersion::Sequoia)
+                descriptor->complete(kIODirectionOut);
         }
 
         hdr = (HciCommandHdr *)hciBuf;
